2c3e6740775745cbf85c6c56b9950ac84d28aa6a
Tom Opdebeeck <tom.opdebeeck@district01.be>
2019-10-18 23:36:13 +0200

    fix: remove hardcoded value

--- src/utils/module.ts
+++ src/utils/module.ts
@@ -3,7 +3,7 @@ import { Path, join } from '@angular-devkit/core';
 
 export const moduleExists = (tree: Tree, { root, module }: { root?: string; module: string; }): boolean => {
     // TODO: use proper module resolution
-    return tree.exists(`${root}/app/atoms/${module}.module.ts`);
+    return tree.exists(`${root}/app/${module}/${module}.module.ts`);
 };
 
 /**
