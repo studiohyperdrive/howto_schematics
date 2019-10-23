f9aa587561557a3a3fedfa865afff4359da5238f
Tom Opdebeeck <tom.opdebeeck@district01.be>
2019-10-23 23:42:44 +0200

    feat: styleguide base setup

--- src/styleguide/index.ts
+++ src/styleguide/index.ts
@@ -1,11 +1,18 @@
 import {
+  apply,
+  applyTemplates,
   chain,
   externalSchematic,
+  mergeWith,
+  move,
   Rule,
   SchematicContext,
   SchematicsException,
   Tree,
+  url,
+  MergeStrategy,
 } from '@angular-devkit/schematics';
+import { strings } from '@angular-devkit/core';
 
 import { ComponentPrefixes } from '../types/component';
 import { readWorkspace } from '../utils/workspace';
@@ -40,6 +47,71 @@ const updateLinter = (): Rule => {
   };
 };
 
+const addDependencies = (): Rule => {
+  return (tree: Tree) => {
+    const packageJson = tree.read('package.json');
+
+    if (!packageJson) {
+      throw new SchematicsException(`Could not find a package.json in workspace`);
+    }
+
+    const packageJsonContents = JSON.parse(packageJson.toString());
+
+    tree.overwrite('package.json', JSON.stringify({
+      ...packageJsonContents,
+      dependencies: {
+        ...packageJsonContents.dependencies,
+        "@angular/cdk": "^8.2.3",
+        "@angular/material": "^8.2.3",
+        "ngx-markdown": "8.2.1",
+      },
+    }, null, 2));
+
+    return tree;
+  };
+};
+
+const setupStyleguide = (): Rule => {
+  return (tree: Tree) => {
+    const workspace = readWorkspace(tree);
+
+    const projectConfig = workspace.projects['styleguide'];
+
+    if (!projectConfig) {
+      throw new SchematicsException(`Could not find project (styleguide) in workspace`);
+    }
+
+    const templates = apply(url(`./files`), [
+      applyTemplates({
+        ...strings,
+        project: workspace.defaultProject,
+      }),
+      move(projectConfig.sourceRoot as string),
+    ]);
+
+    return mergeWith(templates, MergeStrategy.Overwrite);
+  };
+};
+
+
+// import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+
+// const installDependencies = (): Rule => {
+//   return (_tree: Tree, context: SchematicContext) => {
+//     const packages = [
+//       "@angular/cdk@8.*",
+//       "@angular/material@8.*",
+//       "ngx-markdown@8.2.1",
+//     ];
+
+//     packages.forEach((packageName: string) => {
+//       context.addTask(new NodePackageInstallTask({
+//         packageName,
+//       }));
+//     })
+//   };
+// };
+
 export function styleguideSchematic(_options: any): Rule {
   return (_tree: Tree, _context: SchematicContext) => {
     return chain([
@@ -50,12 +122,14 @@ export function styleguideSchematic(_options: any): Rule {
         style: 'scss',
         skipInstall: true,
       }),
+      setupStyleguide(),
       externalSchematic('@schematics/angular', 'library', {
         name: 'ui',
         prefix: 'ui',
         skipInstall: true,
       }),
       updateLinter(),
+      addDependencies(),
     ]);
   };
 }
